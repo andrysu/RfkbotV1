2588002c2f41caae51464acb01c99f1f5f95a1c2
Jarvis2301 <88648954+Jarvis2301@users.noreply.github.com>
2022-02-02 10:57:21 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -26,7 +26,7 @@ const defaultMenu = {
 ┃⬡ Uptime: *%uptime (%muptime)*
 ┃⬡ Database: %rtotalreg dari %totalreg
 ┃⬡ Github:
-┃⬡ https://github.com/AlyaaXd/rfkbot
+┃⬡ https://github.com/Jarvis2301
 ┃
 ┗━━━━━━⬣`.trimStart(),
   header: '┏━━〔 %category 〕━⬣',
@@ -240,10 +240,8 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 ┃⬡ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 ┃⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 ┃
-┃⬡ Script by Alyaa
-┃⬡ Yt : AlyaaXzy
+┃⬡ Script by Katyusha
 ┃⬡ Run bot : Heroku
-┃⬡ Tipe SC rfkbot
 ┗━━━━━━━━⬣`.trim(),
           "buttonText": "Klik Disini",
           "listType": "SINGLE_SELECT",
